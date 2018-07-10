29c7757f04fb187bc232c1ce8d5b9793bf6cb301
antoniyaat <antoniyaata@gmail.com>
2018-07-10 17:13:10 +0300

    Workshop - Portfolio Page build with Bootstrap - Examples

--- JavaScript_For_FrontEnd/02. JavaScript-for-Front-End-Introduction-to-DOM-and-Events/Lab/04_List-Of-Items/list-of-items.js
+++ JavaScript_For_FrontEnd/02. JavaScript-for-Front-End-Introduction-to-DOM-and-Events/Lab/04_List-Of-Items/list-of-items.js
@@ -2,10 +2,20 @@ function addItem() {
     let newElement = document.getElementById('newItemText').value;
     let list = document.getElementById('items');
 
-    if (newElement.length === 0)
-        return;
-
     let listItem = document.createElement('li');
     listItem.textContent = newElement;
     list.appendChild(listItem);
 }
+
+function addItem() {
+    
+    let newItem = document.getElementById('newItemText').value; 
+    let addItem = document.createElement('li');
+    addItem.textContent = newItem;
+
+    // console.dir(newItem);
+    // console.dir(addItem);
+
+      document.getElementById('ítems')
+    .appendChild(addItem.value);
+    // document.body.appendChild(ap
