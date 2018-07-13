b5af4417cb39c338057ebd410e7985728b4ea42a
antoniyaat <antoniyaata@gmail.com>
2018-07-14 01:55:53 +0300

    Upload conditions and solutions for the workshop

---
+++ JavaScript_For_FrontEnd/08. JavaScript-for-Front-End-Workshop/JavaScript-for-Front-End-Workshop/js/custom-functionality.js
@@ -0,0 +1,17 @@
+// $("#nav ul li a").on('click', function(e) {
+//
+//     // prevent default anchor click behavior
+//     e.preventDefault();
+//
+//     // store hash
+//     var hash = this.hash;
+//
+//     // animate
+//     $('html, body').animate({
+//         scrollTop: $(hash).offset().top
+//     }, 300, function(){
+//         window.location.hash = hash;
+//     });
+//
+// });
+//
